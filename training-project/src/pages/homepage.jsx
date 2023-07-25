99aa50bf94b1125a550074717f66d6384e5abacf
gedeastu <gede.astu@gmail.com>
2023-07-25 17:58:47 +0800

    up

--- training-project/src/pages/homepage.jsx
+++ training-project/src/pages/homepage.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from '../components/header'
 import Navbar from '../components/navbar'
+import OurServices from '../components/ourServices'
 const Homepage = () => {
   return (
     <>
     <div className='w-screen h-screen relative'>
     <Navbar/>
     <Header/>
-    
+    <OurServices/>
     </div>
     </>
   )
